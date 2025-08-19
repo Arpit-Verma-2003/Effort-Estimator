f8ea8ea2371861e43f2f099fa13bf969f88f2482
Arpit-Verma-2003 <arpitverma200316@gmail.com>
2025-08-19 22:55:29 +0530

    frontend started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const App = () => {
+  return (
+    <>
+      <h1 className='text-5xl'>Effort Estimator</h1>
+      <p className=''>Turn complex project documents into clear, data-driven estimates in minutes. Upload your BRD or RFQ, choose an estimation technique, and instantly get effort and cost breakdowns — helping you plan smarter, save time, and make confident decisions.</p>
+      <input type="file" />
+    </>
+  )
+}
+
+export default App;
+
