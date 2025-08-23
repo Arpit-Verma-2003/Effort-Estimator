3faa65f42262e7ba52cd93048af45cfa118e3d99
Arpit-Verma-2003 <arpitverma200316@gmail.com>
2025-08-23 23:13:41 +0530

    modifications

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ app = FastAPI(
     description="API to process project documents and estimate software effort",
     version="1.0.0"
 )
-
+origins = [
+    "http://localhost:5173"
+]
 # Optional: Add CORS if frontend will run on different port (e.g., React on localhost:3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For dev use only; in prod, restrict to your frontend domain
+    allow_origins=origins,  # For dev use only; in prod, restrict to your frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
